5d790515b5c0c9d51790ddbb66c23dc2e04aacd7
shotolee <shotolee@gmail.com>
2019-08-18 19:56:34 +0800

    完成了第二页的显示

--- pages/news/news.js
+++ pages/news/news.js
@@ -26,7 +26,8 @@ Page({
     wx.request({
       url: 'https://test-miniprogram.com/api/news/detail', 
       data: {
-        id: this.data.id
+       id: this.data.id
+       // id: '1552623252481'
       },
       success: res => {
         let result = res.data.result
@@ -35,15 +36,26 @@ Page({
         let newssource = result.source
         let newsfirstImage = result.firstImage
         let newsreadCount = result.readCount
-
+        let newsbody = []
 //更新页面数据
+//利用循环读取新闻段落内容
+        for (let i = 0; i < result.content.length; i += 1) {
+          newsbody.push({
+            id: result.content[i].id,
+            text: result.content[i].text,
+            type: result.content[i].type,
+            src: result.content[i].src
+          })
+        }
         this.setData({
           newstitle: newstitle,
           newsdata: newsdata,
           newssource: newssource,
           newsfirstImage: newsfirstImage,
-          newsreadCount: newsreadCount
+          newsreadCount: newsreadCount,
+          newsbody: newsbody
         })
+        console.log(newsbody)
       },
       complete: () => {
         callback && callback()
