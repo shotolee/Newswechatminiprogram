85af4a3648e77d4eae317291a9928088b04068da
shotolee <shotolee@gmail.com>
2019-08-18 19:16:55 +0800

    完成了id跨页传递问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,11 +5,7 @@ Page({
   data: {
     forecast: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他'] ,
     news: [], //下部的新闻列表
-    //热点新闻
-    firstnewsTitle: '',
-    firstnewsSource: '',
-    firstnewsTime: '',
-    id: '',
+    //id: '',
     firstnewsImage: '/images/default.gif' //默认图片
   },
   onPullDownRefresh(){
@@ -20,6 +16,7 @@ Page({
   onLoad() {
     this.getNews()
   },
+  
   getNews(callback){
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list', //接口调用
@@ -30,22 +27,8 @@ Page({
         let result = res.data.result
         //定义下部列表数组
         let news = []
-        //定义热门新闻第一条样式
-        let firstnewsTitle
-        let firstnewsSource
-        let firstnewsTime
-        let id
-        let firstnewsImage
         //利用循环读取新闻内容
         for (let i = 0; i < result.length; i += 1) {
-          // 第一条新闻内容给热门
-          if (i == 0) {
-            id = result[0].id,
-            firstnewsTitle = result[0].title,
-            firstnewsSource = result[0].source,
-            firstnewsTime = util.formatTime(result[0].date),
-            firstnewsImage = result[0].firstImage
-          }  else {
             news.push({
               id: result[i].id,
               title: result[i].title,
@@ -54,17 +37,8 @@ Page({
               firstImage: result[i].firstImage
             })
           }
-        }
-        console.log(result)
       //更新页面数据
         this.setData({
-          //第一条热门新闻
-          firstnewsTitle: firstnewsTitle,
-          firstnewsSource: firstnewsSource,
-          firstnewsTime: firstnewsTime,
-          id: id,
-          firstnewsImage: firstnewsImage,
-          //下部列表，第 2 条至最结束
           news: news
         })
       },
@@ -74,9 +48,9 @@ Page({
       }
     })
   },
-  onTapNews() {
+  onTapNews(event) {
     wx.navigateTo({
-      url: '/pages/news/news?id=' + this.data.id,
+      url: '/pages/news/news?id=' + event.currentTarget.id,
     })
   }
 })
